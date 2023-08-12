84255efc11a34f06d489fbfb5593af12dbde4ad1
Tobiasz-Tomaszewski <tpt484@gmail.com>
2023-08-12 23:34:21 +0200

    This is simple implementation of Hangman game playable in console.

--- Hangman_class.py
+++ Hangman_class.py
@@ -1,13 +1,16 @@
 import string
 
 
-class Word:
+class Hangman:
     def __init__(self, wrd):
         self.w = wrd
         self.status = [1 if v == ' ' else 0 for v in self.w]
         self.guessed_letters = []
         self.word_is_guessed = False
-        self.allowed_symbols = string.ascii_letters + ' '
+        self.allowed_symbols = string.ascii_letters + ' ' + '-'
+        self.hangman_pictures = {}
+        self.bad_tries = 0
+        self.max_tries = 11
         if not self.__word_is_valid():
             raise ValueError("Word contains unsupported signs.")
 
@@ -19,29 +22,33 @@ class Word:
             if self.status[i] == 0:
                 printable += '_'
         if self.word_is_guessed:
-            return f'Congratulation! You have guessed the word! \nYour word: {printable}.\nYour score: {self.calculate_score()}'
-        return f'The letters you have tried: {self.guessed_letters}. \nYour word: {printable}'
-
-    def calculate_score(self):
-        pass
+            return f'Congratulation! You have guessed the word! \nYour word: {printable}.'
+        if self.bad_tries == self.max_tries:
+            return f'{self.hangman_pictures[self.bad_tries]}\nYou lost :(\nThe word was: {self.w}.'
+        if self.hangman_pictures:
+            return f'{self.hangman_pictures[self.bad_tries]}\nLetters you have tried: {self.guessed_letters}.\nYour word: {printable}'
+        return f'Letters you have tried: {self.guessed_letters}.\nYour word: {printable}'
 
     def __word_is_valid(self):
-        for char in self.w:
+        for char in set(self.w):
             if char not in self.allowed_symbols:
                 return False
         return True
 
     def letter_is_valid(self, letter):
-        if letter in self.allowed_symbols:
+        if letter in set(self.allowed_symbols):
             return True
         return False
 
     def __update_status(self, letter):
+        s = sum(self.status)
         for i, v in enumerate(self.w):
             if letter.lower() == v.lower():
                 self.status[i] = 1
         if all(self.status):
             self.word_is_guessed = True
+        if s == sum(self.status):
+            self.bad_tries += 1
         return None
 
     def guess_letter(self, letter):
@@ -51,4 +58,8 @@ class Word:
             return False
         if letter.lower() not in self.guessed_letters:
             self.guessed_letters.append(letter.lower())
+
+    def read_pictures(self, dict_of_pictures):
+        self.hangman_pictures = dict_of_pictures
+        self.max_tries = len(self.hangman_pictures) - 1
 
--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-from hangman_functions import *
 import random
 import sys
-from Word_class import *
+from Hangman_class import *
+import hangman_drawing
 
-f = open('words.txt')
+f = open('Words.txt')
 words = f.readlines()
 f.close()
 words = [word.strip() for word in words]
-
-for word in words:
-    if not word_is_valid(word):
-        print(f"Your 'words.txt' contains not supported sign. Only letters and spacebars are allowed.")
-        sys.exit()
-
 number_of_words = len(words)
-current_word = Word(words[random.randint(0, number_of_words - 1)])
-while not current_word.word_is_guessed:
+current_word = Hangman(words[random.randint(0, number_of_words - 1)])
+current_word.read_pictures(hangman_drawing.drawing)
+
+while (not current_word.word_is_guessed) and (current_word.bad_tries != current_word.max_tries):
     print(current_word)
     user_letter = input("Guess the letter: ")
     if not current_word.letter_is_valid(user_letter):
         print('The letter you have provided is not supported. Please try again.')
         next
     current_word.guess_letter(user_letter)
-    print(current_word)
-
-
+print(current_word)
